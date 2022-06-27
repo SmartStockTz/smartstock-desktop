d64a8abcf7b7950b1771459b4584d566c213ca15
joshuamshana <mama27j@gmail.com>
2022-06-27 23:16:07 +0300

    uodate build file

--- upload.mac.js
+++ upload.mac.js
@@ -2,18 +2,23 @@ const bfast = require('bfast');
 const pkg = require('./package.json');
 const {createReadStream} = require("fs");
 const {join} = require("path");
+const glob = require("glob");
 
 bfast.init({
   applicationId: 'smartstock_lb',
   projectId: 'smartstock'
 });
-bfast.storage().save({
-  filename: `smartstock-${pkg.version}.dmg`,
-  pn: true,
-  data: createReadStream(join(__dirname, 'dist', `smartstock-${pkg.version}.dmg`))
-}, progress => {
-  console.log(progress);
-}).then(console.log).catch(reason => {
-  console.error(reason);
-  process.exit(-1)
-});
+const files = glob.sync('**/dist/*.dmg');
+
+console.log(files);
+
+// bfast.storage().save({
+//   filename: `smartstock-${pkg.version}.dmg`,
+//   pn: true,
+//   data: createReadStream(join(__dirname, 'dist', `smartstock-${pkg.version}.dmg`))
+// }, progress => {
+//   console.log(progress);
+// }).then(console.log).catch(reason => {
+//   console.error(reason);
+//   process.exit(-1)
+// });
