4a108012b4dc781cb69688d853f31f4ce5664baf
joshuamshana <mama27j@gmail.com>
2022-06-28 08:19:18 +0300

    update build file

--- upload.mac.js
+++ upload.mac.js
@@ -1,7 +1,7 @@
 const bfast = require('bfast');
 const pkg = require('./package.json');
-const {createReadStream} = require("fs");
-const {join} = require("path");
+const { createReadStream } = require("fs");
+const { join } = require("path");
 const glob = require("glob");
 
 bfast.init({
@@ -10,15 +10,16 @@ bfast.init({
 });
 const files = glob.sync('**/dist/*.dmg');
 
-console.log(files);
-
-// bfast.storage().save({
-//   filename: `smartstock-${pkg.version}.dmg`,
-//   pn: true,
-//   data: createReadStream(join(__dirname, 'dist', `smartstock-${pkg.version}.dmg`))
-// }, progress => {
-//   console.log(progress);
-// }).then(console.log).catch(reason => {
-//   console.error(reason);
-//   process.exit(-1)
-// });
+if (Array.isArray(files) && files.length > 0) {
+  bfast.storage().save({
+    filename: `smartstock-${pkg.version}.dmg`,
+    pn: true,
+    data: createReadStream(join(__dirname, files[0]))
+  }, progress => {
+    console.log(progress);
+  }).then(console.log).catch(reason => {
+    console.error(reason);
+    process.exit(-1)
+  });
+}
+throw new Error("NO BUILD TO PUBLISH");
--- upload.win.js
+++ upload.win.js
@@ -7,13 +7,18 @@ bfast.init({
   applicationId: 'smartstock_lb',
   projectId: 'smartstock'
 });
-bfast.storage().save({
-  filename: `smartstock_${pkg.version}.exe`,
-  pn: true,
-  data: createReadStream(join(__dirname, 'dist', `smartstock Setup ${pkg.version}.exe`))
-}, progress => {
-  console.log(progress);
-}).then(console.log).catch(reason => {
-  console.error(reason);
-  process.exit(-1)
-});
+const files = glob.sync('**/dist/*.exe');
+
+if (Array.isArray(files) && files.length > 0) {
+  bfast.storage().save({
+    filename: `smartstock-${pkg.version}.exe`,
+    pn: true,
+    data: createReadStream(join(__dirname, files[0]))
+  }, progress => {
+    console.log(progress);
+  }).then(console.log).catch(reason => {
+    console.error(reason);
+    process.exit(-1)
+  });
+}
+throw new Error("NO BUILD TO PUBLISH");
