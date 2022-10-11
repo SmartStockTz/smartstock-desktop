a8b95cb0ddc7bd70f75c9c1ccda7fe11d7e444ca
joshuamshana <mama27j@gmail.com>
2022-10-11 13:03:04 +0300

    show splash

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ if (!gotTheLock) {
     app.quit();
 } else {
     let mainWindow = null;
-    // let splashScreen = null;
+    let splashScreen = null;
 
     const menuTemplateDev = [
         {
@@ -39,41 +39,40 @@ if (!gotTheLock) {
 
     async function createWindow() {
 
-        // splashScreen = new BrowserWindow({
-        //     show: false,
-        //     width: 400,
-        //     maxWidth: 400,
-        //     height: 400,
-        //     maxHeight: 400,
-        //     center: true,
-        //     modal: true,
-        //     frame: false
-        // });
+        splashScreen = new BrowserWindow({
+            show: false,
+            width: 400,
+            maxWidth: 400,
+            height: 400,
+            maxHeight: 400,
+            center: true,
+            modal: true,
+            frame: false
+        });
         mainWindow = new BrowserWindow({
             height: 700,
             width: 1200,
-            // show: false,
+            show: false,
             webPreferences: {
                 preload: path.join(__dirname, 'preload.js')
             }
         });
-
-        // if (splashScreen) {
-        //     await splashScreen.loadFile(__dirname + `/../splash_assets/ssm.png`);
-        //     splashScreen.show();
-        // }
-
-        // mainWindow.webContents.on('dom-ready', () => {
-        //     mainWindow.show();
-        //     if (splashScreen) {
-        //         try {
-        //             splashScreen.close();
-        //         } catch (e) {
-        //             console.log(e);
-        //         }
-        //     }
-        // });
         Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplateDev));
+        if (splashScreen) {
+            await splashScreen.loadFile(__dirname + `/../splash_assets/ssm.png`);
+            splashScreen.show();
+        }
+
+        mainWindow.webContents.on('dom-ready', () => {
+            mainWindow.show();
+            if (splashScreen) {
+                try {
+                    splashScreen.close();
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        });
         mainWindow.webContents.setWindowOpenHandler((handler) => {
             if (handler.disposition === 'new-window') {
                 shell.openExternal(handler.url).catch(_ => ({}));
